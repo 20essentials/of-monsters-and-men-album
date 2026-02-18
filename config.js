1b41db6f7baa93a68d422fa1c36321a221524ea1
DominicDev <s0l0estudi4r@gmail.com>
2026-02-18 18:08:30 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'OF MONSTERS AND MEN';
+export let nameSongs = [
+	"MOUNTAIN SOUND", /*1*/
+	"LITTLE TALKS", /*2*/
+	"DIRTY PAWS", /*3*/
+	"YELLOW LIGHT", /*4*/
+	"LOVE LOVE LOVE", /*5*/
+	"YOUR BONES", /*6*/
+	"SIX WEEKS", /*7*/
+	"SLOOM", /*8*/
+	"HUNGER", /*9*/
+	"SLOW LIFE", /*10*/
+	"WE SINK", /*11*/
+	"WINTER SOUND", /*12*/
+	"BLACK WATER", /*13*/
+	"EMPIRE", /*14*/
+	"I OF THE STORM", /*15*/
+	"CRYSTALS", /*16*/
+	"HUMAN", /*17*/
+	"ORGANS", /*18*/
+	"PHANTOM", /*19*/
+	"VISITOR", /*20*/
+]
